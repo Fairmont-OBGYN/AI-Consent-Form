b7834ebba097176115e94389dd07e05258eda72c
Clifton <clifton3833@gmail.com>
2025-11-11 12:52:37 -0800

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/consent-form-website/",
+  base: "/AI-Consent-Form/",
   plugins: [react()],
 })
